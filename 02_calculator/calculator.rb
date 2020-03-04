64a6b801e81f3d93e78a3ea2a25a48443396ddf8
Anish <anishphadnis.ca@gmail.com>
2020-03-04 18:43:18 -0500

    Updated for mulitplication

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,4 +1,4 @@
-def add (a, b)
+def add(a, b)
     a + b
 end
 
@@ -6,17 +6,31 @@ def subtract(a, b)
     a - b
 end
 
-def sum (array)
+def sum(array)
     sum = 0
 
     if array.length == 0
         sum = 0
+    elsif array.length == 1
+        sum = array[0]
     else 
         array.each do |i|
-            sum += array[i].to_i
+            sum += i
         end
     end
 
     sum
+
+end
+
+def multiply(array)
+
+    product = 1
+
+    array.each do |i|
+        product *=  i
+    end 
+
+    product
 
 end
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -77,13 +77,19 @@ end
 # once the above tests pass,
 # write tests and code for the following:
 
-describe "#multiply" do
+describe "multiply" do
+  it "multiplies two numbers" do
+    expect(multiply([2,3])).to eq(6)
+  end 
 
-  it "multiplies two numbers"
-
-  it "multiplies several numbers"
-  
+  it "multiplies several numbers" do
+    expect(multiply([2,3,4,5])).to eq(120)
+  end
 end
+
+
+  
+
 
 describe "#power" do
   it "raises one number to the power of another number"
